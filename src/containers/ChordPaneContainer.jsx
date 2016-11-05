6eb5f8acb6b500b9e378a0c597f1b622d33f22a1
Myers Carpenter <myers@teamtreehouse.com>
2016-11-05 01:50:05 -0400

    remove console.log

--- src/containers/ChordPaneContainer.jsx
+++ src/containers/ChordPaneContainer.jsx
@@ -1,11 +1,8 @@
 import { connect } from 'react-redux';
 import ChordPane from '../components/ChordPane.jsx';
 
-const mapStateToProps = state => {
-  console.log("state in mapStateToProps", state)
-  return {
-    chord: state.chordDrill.listeningForChord.rootNote
-  }
-}
+const mapStateToProps = state => ({
+  chord: state.chordDrill.listeningForChord.rootNote
+})
 
 export default connect(mapStateToProps)(ChordPane)
